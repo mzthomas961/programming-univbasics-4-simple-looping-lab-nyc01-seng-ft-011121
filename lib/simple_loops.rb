c424b7950b4acf4cc8843507ac4e5296231605c3
Matthew Thomas <mzthomas96@gmail.com>
2021-01-02 18:18:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,4 +22,11 @@ while count < array.length do
   count += 1
 end
 end
-
+def return_string_array(array)
+count = 0
+while count < array.length do
+  array[count] = array[count].to_s
+  count += 1
+    end
+return array
+    end
